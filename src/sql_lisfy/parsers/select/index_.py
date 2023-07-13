42fe1c5a3d9028037ba2867a68426db866004e37
Naoya Yamashita <conao3@gmail.com>
2023-07-13 13:17:21 +0900

    implement select list parser

--- src/sql_lisfy/parsers/select/index_.py
+++ src/sql_lisfy/parsers/select/index_.py
@@ -9,32 +9,56 @@ from ... import parser_subr
 TOKENS = []
 
 
-def read_all_distinct(input_stream: more_itertools.peekable[types.Token], arg: dict[str, Any]) -> None:
-    peek = input_stream.peek(None)
-    if peek is None:
-        return
+def read_all_distinct(input_stream: more_itertools.peekable[types.Token], args: dict[str, Any]) -> None:
+    if parser_subr.expect_token(input_stream, 'ALL'):
+        args['all'] = True
 
-    if peek.name.upper() == 'ALL':
-        _ = next(input_stream)  # consume ALL
-        arg['all'] = True
+    if parser_subr.expect_token(input_stream, 'DISTINCT'):
+        args['distinct'] = True
 
-    elif peek.name.upper() == 'DISTINCT':
-        _ = next(input_stream)  # consume DISTINCT
-        arg['distinct'] = True
-
-        peek = input_stream.peek(None)
-        if peek is None:
-            return
-
-        if peek.name.upper() == 'ON':
-            _ = next(input_stream)  # consume ON
+        if parser_subr.expect_token(input_stream, 'ON'):
             exps = parser_subr.read_expr_list(input_stream)
-            arg['distinct_on'] = exps
+            args['distinct_on'] = exps
+
+
+def read_select_list(input_stream: more_itertools.peekable[types.Token], args: dict[str, Any]) -> None:
+    res: list[types.Expression] = []
+    while True:
+        exp = parser_subr.read_expr(input_stream)
+
+        if parser_subr.expect_token(input_stream, 'AS'):
+            exp.args['alias'] = next(input_stream).name
+
+        res.append(exp)
+
+        if (
+            parser_subr.expect_token(input_stream, 'FROM') or
+            parser_subr.expect_token(input_stream, 'WHERE') or
+            parser_subr.expect_token(input_stream, 'GROUP') or
+            parser_subr.expect_token(input_stream, 'HAVING') or
+            parser_subr.expect_token(input_stream, 'WINDOW') or
+            parser_subr.expect_token(input_stream, 'UNION') or
+            parser_subr.expect_token(input_stream, 'INTERSECT') or
+            parser_subr.expect_token(input_stream, 'EXCEPT') or
+            parser_subr.expect_token(input_stream, 'ORDER') or
+            parser_subr.expect_token(input_stream, 'LIMIT') or
+            parser_subr.expect_token(input_stream, 'OFFSET') or
+            parser_subr.expect_token(input_stream, 'FETCH') or
+            parser_subr.expect_token(input_stream, 'FOR') or
+            parser_subr.expect_eof(input_stream)
+        ):
+            break
+
+        parser_subr.ensure_token(input_stream, ',')
+
+    if res:
+        args['select_list'] = res
 
 
 def read(input_stream: more_itertools.peekable[types.Token]) -> types.Statement:
     args = {}
     read_all_distinct(input_stream, args)
+    read_select_list(input_stream, args)
 
     return types.Statement(
         name='SELECT',
